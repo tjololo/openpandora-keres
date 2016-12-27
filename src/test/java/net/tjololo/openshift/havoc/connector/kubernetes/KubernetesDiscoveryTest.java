da8934f9c4c140d17e6bb2dc26837bb787c2de84
tjololo <vemundg@gmail.com>
2016-12-27 22:37:56 +0100

    Improve code quality. Implementet simple test

--- src/test/java/net/tjololo/openshift/havoc/connector/kubernetes/KubernetesDiscoveryTest.java
+++ src/test/java/net/tjololo/openshift/havoc/connector/kubernetes/KubernetesDiscoveryTest.java
@@ -1,10 +1,33 @@
 package net.tjololo.openshift.havoc.connector.kubernetes;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.web.client.RestTemplate;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by veg on 19.12.2016.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class KubernetesDiscoveryTest {
     private KubernetesDiscovery kubernetesDiscovery;
+    @Mock
+    private RestTemplate restTemplate;
+    private static final String TOKEN="22Token22";
 
+    @Before
+    public void setUp() {
+        kubernetesDiscovery = new KubernetesDiscovery(restTemplate, TOKEN);
+    }
 
+    @Test
+    public void check_that_kubernetesDiscovery_not_null() throws Exception {
+        assertThat(kubernetesDiscovery, not(nullValue()));
+    }
 }
