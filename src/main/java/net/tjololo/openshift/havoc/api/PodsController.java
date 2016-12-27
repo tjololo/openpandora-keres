b75943f26ca786efd48f19b25632f9bf0712dd10
tjololo <vemundg@gmail.com>
2016-12-27 22:13:08 +0100

    Improve code quality. Remove unused imprts

--- src/main/java/net/tjololo/openshift/havoc/api/PodsController.java
+++ src/main/java/net/tjololo/openshift/havoc/api/PodsController.java
@@ -3,7 +3,6 @@ package net.tjololo.openshift.havoc.api;
 import net.tjololo.openshift.havoc.api.v1.Pod;
 import net.tjololo.openshift.havoc.api.v1.Status;
 import net.tjololo.openshift.havoc.connector.kubernetes.KubernetesDiscovery;
-import net.tjololo.openshift.havoc.connector.kubernetes.contracts.v1.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
